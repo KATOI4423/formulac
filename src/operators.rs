aef0310d51bbb49ea85277172f8028027bd9f0ee
KATOI4423 <katoi4423@gmail.com>
2026-02-19 22:23:02 +0900

    feat: Add binary operator kinds (Add, Sub, Mul, Div, Pow)

--- src/operators.rs
+++ src/operators.rs
@@ -2,7 +2,14 @@
 
 use crate::functions::core::ComplexBackend;
 
-use std::ops::Neg;
+use std::ops::{
+    Neg,
+    Add,
+    Sub,
+    Mul,
+    Div,
+};
+
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub(crate) enum ParseOperatorError {
@@ -55,6 +62,79 @@ unary_operator_kinds! {
     Neg => { symbol: "-", apply: |x: T| -x },
 }
 
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub(crate) struct BinaryOperatorInfo {
+    pub precedence: u8,
+    pub is_left_assoc: bool,
+}
+
+macro_rules! binary_operator_kinds {
+    ($( $name:ident => {
+        symbol: $symbol:expr,
+        precedence: $prec:expr,
+        left_assoc: $assoc:expr,
+        apply: $apply:expr
+    }), + $(,)?) => {
+        #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+        pub(crate) enum BinaryOperatorKind {
+            $( $name ),*
+        }
+
+        impl BinaryOperatorKind {
+            pub fn info(&self) -> BinaryOperatorInfo
+            {
+                match self {
+                    $(
+                        Self::$name => BinaryOperatorInfo{ precedence: $prec, is_left_assoc: $assoc }
+                    ), +
+                }
+            }
+
+            pub fn apply<T, S>(&self, l: T, r: T) -> T
+            where
+                T: ComplexBackend<S>
+                    + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T>,
+                S: Clone + Send + Sync + 'static,
+            {
+                match self {
+                    $(
+                        Self::$name => $apply(l, r)
+                    ), +
+                }
+            }
+
+            pub fn names() -> &'static[&'static str]
+            {
+                &[ $( $symbol ), + ]
+            }
+        }
+
+        impl std::str::FromStr for BinaryOperatorKind {
+            type Err = ParseOperatorError;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err>
+            {
+                match s {
+                    $(
+                        $symbol => Ok(Self::$name),
+                    ) +
+                    _ => Err(Self::Err::UnknownOperator),
+                }
+            }
+        }
+    };
+}
+
+binary_operator_kinds! {
+    Add => { symbol: "+", precedence: 0, left_assoc: true,  apply: |l: T, r: T| l + r },
+    Sub => { symbol: "-", precedence: 0, left_assoc: true,  apply: |l: T, r: T| l - r },
+    Mul => { symbol: "*", precedence: 1, left_assoc: true,  apply: |l: T, r: T| l * r },
+    Div => { symbol: "/", precedence: 1, left_assoc: true,  apply: |l: T, r: T| l / r },
+    Pow => { symbol: "^", precedence: 2, left_assoc: true,  apply: |l: T, r: T| l.pow(&r) },
+}
+
+
 #[cfg(test)]
 mod unary_operator_kind_tests {
     use super::*;
@@ -76,5 +156,25 @@ mod unary_operator_kind_tests {
         let x = Complex::new(2.0, -1.0);
         assert_eq!(UnaryOperatorKind::Pos.apply(x), x);
         assert_eq!(UnaryOperatorKind::Neg.apply(x), -x);
+    }
+}
+
+#[cfg(test)]
+mod binary_operator_kinds_tests {
+    use super::*;
+
+    use std::str::FromStr;
+
+    #[test]
+    fn test_binary_operator_kind_from() {
+        assert_eq!(BinaryOperatorKind::from_str("+"), Ok(BinaryOperatorKind::Add));
+        assert_eq!(BinaryOperatorKind::from_str("-"), Ok(BinaryOperatorKind::Sub));
+        assert_eq!(BinaryOperatorKind::from_str("*"), Ok(BinaryOperatorKind::Mul));
+        assert_eq!(BinaryOperatorKind::from_str("/"), Ok(BinaryOperatorKind::Div));
+        assert_eq!(BinaryOperatorKind::from_str("^"), Ok(BinaryOperatorKind::Pow));
+
+        assert!(BinaryOperatorKind::from_str("").is_err());
+        assert!(BinaryOperatorKind::from_str("x").is_err());
+        assert!(BinaryOperatorKind::from_str("%").is_err());
     }
 }
